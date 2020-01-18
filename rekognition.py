b171ddfa8cb67767c4a240659a25eff9068b1c7e
anujdev11 <adev23452@gmail.com>
2020-01-18 18:48:31 +0530

    final_aadhar

--- rekognition.py
+++ rekognition.py
@@ -16,8 +16,22 @@ for text in textDetections:
     if(text['Confidence']>85):
         if(text['Type']=='LINE' and re.search(r'\w* \w*',text['DetectedText'])):
             if('DOB' in text['DetectedText']):
-                print(text['DetectedText'])
-
-
-        # print ('Type:' + text['Type'])
-# print(a)
+                print('Date of Birth:',text['DetectedText'][text['DetectedText'].find('DOB')+5:])
+                DoB1=text['DetectedText'][text['DetectedText'].find('DOB')+5:]
+                if(DoB1!=""):
+                        DoB=DoB1
+        if('MALE' in text['DetectedText']):
+            print("Gender: Male")
+            gender="Male"
+        elif('FEMALE' in text['DetectedText']):
+            print("Gender: Female")
+            gender="Female"
+        if(re.match(r'\d{4} \d{4} \d{4}',text['DetectedText']) and text['Type']=='LINE'):
+            print('AADhar number:' + text['DetectedText'])
+            aadhar_no=text['DetectedText']
+        if(re.search(r'[A-Z][a-z]{1,} [A-Z][a-z]{1,}\s*[a-zA-Z]*',text['DetectedText']) and 'GOV' not in text['DetectedText'] and 'Gov' not in text['DetectedText'] and ":" not in text['DetectedText'] and text['Type']=='LINE'):
+            print("Name:",text['DetectedText'])
+            name=text['DetectedText']
+        if(re.match('\d{10}',text['DetectedText'])):
+            print("Mobile_no:",text['DetectedText'])
+            mobile_no=text['DetectedText']
