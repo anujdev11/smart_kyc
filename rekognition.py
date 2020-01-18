e1f3f0a401de7f729e6ad11bec41dbc9c8ddde0d
anujdev11 <adev23452@gmail.com>
2020-01-18 18:32:46 +0530

    new try

--- rekognition.py
+++ rekognition.py
@@ -1,15 +1,23 @@
 import boto3
+import re
 client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id='AKIAJUNDV4AEW3TWKQ6Q',
                       aws_secret_access_key='XUAM1Ngz8MsXHB9FS8Nc5ttPxItU6/DWLdg7YPS2')
 
-photo="sample1_back.jpg"
+photo="sample.jpeg"
 with open(photo, "rb") as imageFile:
         response = client.detect_text(Image={'Bytes': imageFile.read()})
         textDetections = response['TextDetections']
-        # print(textDetections[0][''])
+        # print(textDetections)
+        # if(text['Confidence']>85):
+
 
 a=[]
-for i in textDetections:
-    a.append(i['DetectedText'])
+for text in textDetections:
+    if(text['Confidence']>85):
+        if(text['Type']=='LINE' and re.search(r'\w* \w*',text['DetectedText'])):
+            if('DOB' in text['DetectedText']):
+                print(text['DetectedText'])
 
-print(a)
+
+        # print ('Type:' + text['Type'])
+# print(a)
