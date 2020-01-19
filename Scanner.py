35a23818135d97eb0c6797e6f821e9b94dcf93e7
anujdev11 <adev23452@gmail.com>
2020-01-19 09:31:43 +0530

    final_kyc

--- Scanner.py
+++ Scanner.py
@@ -1,32 +1,63 @@
-import cv2
 import numpy as np
-#import mapper
-#import Adhar
+import cv2
 import os
 import re
 import boto3
 
 class AdharExtract:
-        def getData(self,img1):
-                
+        def getData(self,img1,img2):        
                 image=cv2.imread(img1)   #read in the image
                 image=cv2.resize(image,(1000,800)) #resizing because opencv does not work well with bigger images
                 orig=image.copy()
                 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
-                
                 blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
-                
                  #storing processed image temporarily
                 cv2.imwrite('processed_image.png',blurred)
 
-                client=boto3.client('rekognition',region_name='your_region',aws_access_key_id='your_aws_access_key_id',
-                aws_secret_access_key='your_aws_secret_access_key')
+
+                image=cv2.imread(img2)   #read in the image
+                image=cv2.resize(image,(1000,800)) #resizing because opencv does not work well with bigger images
+                orig=image.copy()
+                gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+                blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+                 #storing processed image temporarily
+                cv2.imwrite('processed_image1.png',blurred)
+
+                client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id='AKIAJUNDV4AEW3TWKQ6Q',
+                      aws_secret_access_key='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')
+                client1 = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id='AKIAJUNDV4AEW3TWKQ6Q',
+                      aws_secret_access_key='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')
                  
                 with open("processed_image.png", "rb") as imageFile:	
                         response=client.detect_text(Image={'Bytes': imageFile.read()})
+                 
+                with open("processed_image1.png", "rb") as imageFile:	
+                        response1=client1.detect_text(Image={'Bytes': imageFile.read()})
+                        
+                                                                        
+                        textDetections1=response1['TextDetections']
+
+                        for text in textDetections1:
+                                print('Detected text:' + text['DetectedText'])
+                        
+                        address=""
+                        a=[]
+                        for text in textDetections1:
+                                if(text['Confidence']>85 and text['Type']=="LINE" ):
+                                        a.append(text['DetectedText'])
+
+                                #print(text['DetectedText'])
+                        for i in range(len(a)):
+                                if((a[i]=='Address:') or (a[i]=='Address :') or (a[i]=='Address') or (a[i]=='Address ')):
+                                        while(1):
+                                                address=address+a[i+1]
+                                                i=i+1
+                                                if((a[i+1]=='www') or (a[i+1]=='wwW') or (a[i+1]=='wWW') or (a[i+1]=='WWW')  or (a[i+1]=='wWw')):
+                                                        break
+                        
                                                                         
                         textDetections=response['TextDetections']
-                        print ('Detected text')
+                        # print ('Detected text')
                         name,gender,aadhar_no,mobile_no,DoB="","","","",""
                         for text in textDetections:
                                 print('Detected text:' + text['DetectedText'])
@@ -35,15 +66,15 @@ class AdharExtract:
 
                                 if(text['Confidence']>85):
                                         if(text['Type']=='LINE' and re.search(r'\w* \w*',text['DetectedText'])):
-                                                if('DOB' in text['DetectedText']):
+                                                if('DOB' in text['DetectedText'] or 'DOB' in text['DetectedText']):
                                                         print('Date of Birth:',text['DetectedText'][text['DetectedText'].find('DOB')+5:])
                                                         DoB1=text['DetectedText'][text['DetectedText'].find('DOB')+5:]
                                                         if(DoB1!=""):
                                                                 DoB=DoB1
-                                        if('MALE' in text['DetectedText']):
+                                        if('MALE' in text['DetectedText'] or 'Male' in text['DetectedText']  ):
                                                         print("Gender: Male")
                                                         gender="Male"
-                                        elif('FEMALE' in text['DetectedText']):
+                                        elif('FEMALE' in text['DetectedText'] or 'Female' in text['DetectedText']):
                                                         print("Gender: Female")
                                                         gender="Female"
                                         if(re.match(r'\d{4} \d{4} \d{4}',text['DetectedText'])and text['Type']=='LINE'):
@@ -56,8 +87,11 @@ class AdharExtract:
                                                 print("Mobile_no:",text['DetectedText'])
                                                 mobile_no=text['DetectedText']
                                         
-                                        print ('Type:' + text['Type'])
-                return (name,gender,aadhar_no,mobile_no,DoB)
+                return (name,gender,aadhar_no,mobile_no,DoB,address)
+        # def getaddress(self,img2):
+
+
+
 
 if __name__=='__main__':
         s=sc()
--- rekognition_back.py
+++ rekognition_back.py
@@ -3,7 +3,7 @@ import re
 client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id='AKIAJUNDV4AEW3TWKQ6Q',
                       aws_secret_access_key='XUAM1Ngz8MsXHB9FS8Nc5ttPxItU6/DWLdg7YPS2')
 
-photo="sample_arun.jpg"
+photo="sample1.jpg"
 with open(photo, "rb") as imageFile:
         response = client.detect_text(Image={'Bytes': imageFile.read()})
         textDetections = response['TextDetections']
@@ -13,18 +13,21 @@ with open(photo, "rb") as imageFile:
 
 a=[]
 for text in textDetections:
-    if(text['Confidence']>85 and text['Type']=="LINE" ):
-       a.append(text['DetectedText'])
+    if(text['Confidence']>85 and text['Type']=="LINE"):
+        a.append(text['DetectedText'])
 
     #    print(text['DetectedText'])
+
 a1=""
 for i in range(len(a)):
-    if((a[i]=='Address:') or (a[i]=='Address :')):
-        while(1):
-            print(a[i+1])
-            a1=a1+a[i+1]
-            i=i+1
-            if(a[i+1]=='www'):
-                break
-print(a)
+    a1+=a[i]
+    # if((a[i]=='Address:') or (a[i]=='Address :')):
+        # while(1):
+        #     a1=a1+a[i+1]
+        #     i=i+1
+
+            # if(re.match(r'\d{6}$',a[i+1])):
+            #     break
+# print(a)
+a1=re.search(r"(^Address:((?! \d+ ).)(, A-za-z0-9 ,)\d+(, \d{6}| \d{6}|\b ))",a1)
 print(a1)
